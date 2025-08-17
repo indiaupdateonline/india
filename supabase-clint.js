2d7482d5c3b931ac6f1fdcaacc9e10909114d13c
indiaupdateonline <versiasite@gmail.com>
2025-08-17 17:02:56 +0530

    Delete supabase-clint.js

--- supabase-clint.js
+++
@@ -1,9 +0,0 @@
-// assets/js/supabase-client.js
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-export const SUPABASE_URL = 'https://frnzwjwdlymvxzjnbzfn.supabase.co';
-export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZybnp3andkbHltdnh6am5iemZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNzY5MDksImV4cCI6MjA3MDg1MjkwOX0.HbfY-_PPhXYXccmqjqBrIQm4LGcItzDre3rbLQACb5o';
-
-
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
